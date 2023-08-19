67037292e8a699b9fd233cac4d7113ffbc9589e1
liujia <liujia26@zuoyebang.com>
2023-08-19 17:48:02 +0800

    test

--- scripts/wexperimental.py
+++ scripts/wexperimental.py
@@ -10,17 +10,43 @@ import numpy as np
 class Script(scripts.Script):
 
     def title(self):
-        print('**************title')
-        return "WExperimental"
-        
-    def show(self):
-        print('**************show')
-        return True
+        print('----------------custom extension titile')
+        return "Extension Template"
 
-    def ui(self):
-        print('**************ui')
-        return None
+    # Decide to show menu in txt2img or img2img
+    # - in "txt2img" -> is_img2img is `False`
+    # - in "img2img" -> is_img2img is `True`
+    #
+    # below code always show extension menu
+    def show(self, is_img2img):
+        print('----------------custom extension show')
+        return scripts.AlwaysVisible
 
-    def run(self, p, only_save_background_free_pictures, do_not_auto_save):
-        print('**************run')
-        return None
+    # Setup menu ui detail
+    def ui(self, is_img2img):
+        print('----------------custom extension ui')
+        with gr.Accordion('Extension Template', open=False):
+            with gr.Row():
+                angle = gr.Slider(
+                    minimum=0.0,
+                    maximum=360.0,
+                    step=1,
+                    value=0,
+                    label="Angle"
+                )
+                checkbox = gr.Checkbox(
+                    False,
+                    label="Checkbox"
+                )
+        # TODO: add more UI components (cf. https://gradio.app/docs/#components)
+        return [angle, checkbox]
+
+    # Extension main process
+    # Type: (StableDiffusionProcessing, List<UI>) -> (Processed)
+    # args is [StableDiffusionProcessing, UI1, UI2, ...]
+    def run(self, p, angle, checkbox):
+        print('----------------custom extension run')
+        # TODO: get UI info through UI object angle, checkbox
+        proc = process_images(p)
+        # TODO: add image edit process via Processed object proc
+        return proc
