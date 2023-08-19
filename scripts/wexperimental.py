9a506a96e79e76e0a40f22258d25add99e20337e
liujia <liujia26@zuoyebang.com>
2023-08-19 16:54:21 +0800

    add log

--- scripts/wexperimental.py
+++ scripts/wexperimental.py
@@ -10,13 +10,17 @@ import numpy as np
 class Script(scripts.Script):
 
     def title(self):
+        print('**************title')
         return "WExperimental"
         
     def show(self):
+        print('**************show')
         return True
 
     def ui(self):
+        print('**************ui')
         return None
 
     def run(self, p, only_save_background_free_pictures, do_not_auto_save):
+        print('**************run')
         return None
